24a5785d291f25cde196da63bb65e0f53d2f9e35
Nico <lawani.gk@gmail.com>
2023-06-28 01:43:12 +0100

    modify 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,8 +4,5 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return { status: 200, body: 'Success' };
     })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+    .catch(() => new Error());
 }
