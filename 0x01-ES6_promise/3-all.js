d9c177816109c59c5ff11d01e5f14ad288d5bb94
Nico <lawani.gk@gmail.com>
2023-06-28 01:59:11 +0100

    add error handling

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,11 @@
 import { createUser, uploadPhoto } from './utils.js';
 
 export default async function handleProfileSignup() {
-  const user = await createUser();
-  const upload = await uploadPhoto();
-  console.log(upload.body, Object.values(user).join(' '));
+  try {
+    const user = await createUser();
+    const upload = await uploadPhoto();
+    console.log(upload.body, Object.values(user).join(' '));
+  } catch {
+    console.log('Signup system offline');
+  }
 }
