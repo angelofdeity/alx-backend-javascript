63ae9029e26a223d551f0ba40d680486b8d06638
Nico <lawani.gk@gmail.com>
2023-06-28 11:35:19 +0100

    modify 2-then to fix bug where no return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { createUser, uploadPhoto } from './utils.js';
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
   try {
@@ -7,4 +7,3 @@ export default function handleProfileSignup() {
     console.log('Signup system offline');
   }
 }
-handleProfileSignup();
