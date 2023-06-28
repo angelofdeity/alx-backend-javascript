97076c83adcf689fbcb1f78ddd8e5cca1f04e1ad
Nico <lawani.gk@gmail.com>
2023-06-28 02:07:13 +0100

    improve 3-all by using Promise.all method

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,8 @@
 import { createUser, uploadPhoto } from './utils.js';
 
-export default async function handleProfileSignup() {
-  try {
-    const user = await createUser();
-    const upload = await uploadPhoto();
-    console.log(upload.body, Object.values(user).join(' '));
-  } catch {
-    console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  Promise.all([createUser(), uploadPhoto()])
+    .then(([user, upload]) => console.log(upload.body, Object.values(user).join(' ')))
+    .catch(() => console.log('Signup system offline'));
 }
+handleProfileSignup();
