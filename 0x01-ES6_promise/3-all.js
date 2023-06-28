a96859ebb33c627dc013d371021083adf65cdff0
Nico <lawani.gk@gmail.com>
2023-06-28 03:57:32 +0100

    refactor code to improve err handling

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,10 @@
 import { createUser, uploadPhoto } from './utils.js';
 
 export default function handleProfileSignup() {
-  Promise.all([createUser(), uploadPhoto()])
-    .then(([user, upload]) => console.log(upload.body, Object.values(user).join(' ')))
-    .catch(() => console.log('Signup system offline'));
+  try {
+    Promise.all([createUser(), uploadPhoto()]).then(([user, upload]) => console.log(upload.body, user.firstName, user.lastName));
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
+handleProfileSignup();
