770603f0be26c816d1784ae5b82ee0d7d6962f4d
Nico <lawani.gk@gmail.com>
2023-06-28 15:22:56 +0100

    modify 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]).then(
     (results) => results.map((result) => ({
       status: result.status,
-      value: result.value || result.reason.message,
+      value: result.value || `Error: ${result.reason.message}`,
     })),
   );
 }
