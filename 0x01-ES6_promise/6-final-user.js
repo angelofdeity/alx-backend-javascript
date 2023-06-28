5f9856bdfd396b666a39bba9588f33499514627a
Nico <lawani.gk@gmail.com>
2023-06-28 03:18:04 +0100

    update 6-final-user to capture error msgs

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]).then(
-    (results) => results.map((result) => ({ status: result.status, value: result.value || result.reason })),
+    (results) => results.map((result) => ({
+      status: result.status,
+      value: result.value || result.reason.message,
+    })),
   );
 }
---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,4 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  return queue;
+}
