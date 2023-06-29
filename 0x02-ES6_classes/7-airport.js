98770e6090af3ed083fdc5805d337f583db1a48f
Nico <lawani.gk@gmail.com>
2023-06-29 15:34:45 +0100

    remove log statements

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -8,7 +8,3 @@ export default class Airport {
     return `${this._code}`;
   }
 }
-
-const airportSF = new Airport('San Francisco Airport', 'SFO');
-console.log(airportSF);
-console.log(airportSF.toString());
