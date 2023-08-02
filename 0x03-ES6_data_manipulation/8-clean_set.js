5416af504486a5fe89b99e1735c3499967729dff
Nico <lawani.gk@gmail.com>
2023-08-02 17:51:01 +0100

    add check if input is string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,9 @@
 export default function cleanSet(mySet, myStr) {
-  // if (!myStr) {
-  //   return '';
-  // }
-  return [...mySet]
-    .filter((elem) => elem && myStr && elem.startsWith(myStr))
-    .map((elem) => (elem ? elem.slice(myStr.length) : ''))
-    .join('-');
+  if (myStr instanceof String) {
+    return [...mySet]
+      .filter((elem) => elem && myStr && elem.startsWith(myStr))
+      .map((elem) => (elem ? elem.slice(myStr.length) : ''))
+      .join('-');
+  }
+  return '';
 }
-// export default function cleanSet(mySet, myStr) {
-//   const newArr = [];
-//   mySet.forEach((element) => {
-//     if (myStr && element.includes(myStr)) {
-//       newArr.push(element.replace(myStr, ''));
-//     }
-//   });
-//   return newArr.join('-');
-// }
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), undefined));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
