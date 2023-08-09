151d71064e66a9d0a672135f2752f8b113779db9
Nico <lawani.gk@gmail.com>
2023-08-09 12:57:23 +0100

    Build full server following mvc approach

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,45 @@
+import readDatabase from '../utils';
+
+const path = require('path');
+
+const filePath = path.join(__dirname, '../../database.csv');
+export default class StudentsController {
+  static getAllStudents(req, res) {
+    res.statusCode = 200;
+    readDatabase()
+      .then((data) => {
+        const response = [
+          'This is the list of our students',
+          `Number of students in CS: ${data.cs.length}`,
+          `Number of students in SWE: ${data.swe.length}`,
+        ].join('\n');
+        res.end(response);
+      })
+      .catch(() => {
+        res.statusCode = 500;
+        res.end('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(req, res) {
+    res.statusCode = 200;
+    const param = req.params.major;
+    console.log(param);
+    if (!['CS', 'SWE'].includes(param)) {
+      res.statusCode = 500;
+      res.end('Major parameter must be CS or SWE');
+    }
+    readDatabase(filePath)
+      .then((data) => {
+        if (param === 'CS') {
+          res.end(`List: ${data.cs}`);
+        } else if (param === 'SWE') {
+          res.end(`List: ${data.swe}`);
+        }
+      })
+      .catch(() => {
+        res.statusCode = 500;
+        res.end('Cannot load the database');
+      });
+  }
+}
