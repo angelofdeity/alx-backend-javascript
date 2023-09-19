50e10f4953c5032b5fe4ca678cf1f049f344f4c7
Nico <lawani.gk@gmail.com>
2023-09-19 11:06:13 +0100

    modify 7-http-express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -3,34 +3,35 @@ const express = require('express');
 
 const app = express();
 const port = 1245;
-const file = process.argv[2];
 
 function handleData(data) {
-  const lines = data.split('\n').filter((line) => line !== 'firstname,lastname,age,field');
+  const lines = data.split('\n').filter((line) => line !== '');
+  const headers = lines.shift().split(',');
+  const fieldIndex = headers.indexOf('field');
   const cs = [];
   const swe = [];
   const students = lines.map((line) => line.split(','));
   for (const student of students) {
     if (student) {
-      if (student[3] === 'CS') {
+      if (student[fieldIndex] === 'CS') {
         cs.push(student[0]);
-      } else if (student[3] === 'SWE') {
+      } else if (student[fieldIndex] === 'SWE') {
         swe.push(student[0]);
       }
     }
   }
 
   return (
-    `Number of students: ${cs.length + swe.length}\n`
+    `Number of students: ${students.length}\n`
     + `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}\n`
     + `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`
   );
 }
-function countStudents(filePath) {
+async function countStudents(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (error, data) => {
       if (error) {
-        reject(new Error('Cannot load database'));
+        reject(Error('Cannot load database'));
       } else {
         resolve(handleData(data));
       }
@@ -41,7 +42,12 @@ app.get('/', (req, res) => {
   res.end('Hello Holberton School!');
 });
 app.get('/students', async (req, res) => {
-  res.end(`This is the list of our students\n${await countStudents(file)}`);
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${data}`);
+  } catch (err) {
+    res.send(`This is the list of our students\n${err.message}`);
+  }
 });
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
